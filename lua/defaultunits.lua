8f2954097b1e34474d25ad8292d8100ce36fa954
IceDreamer <ben-knight@bluebottle.com>
2015-10-17 21:44:30 +0100

    Add ACU class version of the function

--- lua/defaultunits.lua
+++ lua/defaultunits.lua
@@ -2327,6 +2327,24 @@ ACUUnit = Class(CommandUnit) {
         self:GetAIBrain():GiveResource('Energy', self:GetBlueprint().Economy.StorageEnergy)
         self:GetAIBrain():GiveResource('Mass', self:GetBlueprint().Economy.StorageMass)
     end,
+    
+    -- Called when this unit kills another. Chiefly responsible for the veterancy system for now.
+    OnKilledUnit = function(self, unitKilled, massKilled)
+        if not massKilled then return end -- Make sure engine calls aren't passed with massKilled == 0
+        
+        if not IsAlly(self:GetArmy(), unitKilled:GetArmy()) then
+        
+            -- Adjust mass based on unit killed for ACU
+            local techIndex = function()
+                for k, tech in pairs({'TECH1', 'TECH2', 'TECH3', 'EXPERIMENTAL', 'COMMAND'}) do
+                    if tech == unitKilled.techLevel then return k end
+                end
+            end
+            massKilled = massKilled / techIndex()
+            
+            self:CalculateVeterancyLevel(massKilled) -- Bails if we've not gone up
+        end
+    end,
 
     BuildDisable = function(self)
         while self:IsUnitState('Building') or self:IsUnitState('Enhancing') or self:IsUnitState('Upgrading') or
