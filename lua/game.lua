58a263e02fdfea1035368e184d8aa7b0edb740fa
IceDreamer <ben-knight@bluebottle.com>
2017-02-26 08:52:08 +0000

    Optimize math.max

--- lua/game.lua
+++ lua/game.lua
@@ -5,6 +5,8 @@
 -- Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
 -----------------------------------------------------------------
 
+-- Localized for speed
+local mathMax = math.max
 
 FireState = {
     RETURN_FIRE = 0,
@@ -68,8 +70,6 @@ BuffFieldBlueprint { -- Seraphim ACU Advanced Restoration
 -- In order to keep backwards compatibility, there is a new option in the blueprint economy section.
 -- if DifferentialUpgradeCostCalculation is set to true, the base upgrade cost will be subtracted
 function GetConstructEconomyModel(builder, targetData, upgradeBaseData)
-      mass = math.max(mass - upgradeBaseData.BuildCostMass, 0)
-      energy = math.max(energy - upgradeBaseData.BuildCostEnergy, 0)
     -- 'rate' here is how fast we build relative to a unit with build rate of 1
     local rate = builder:GetBuildRate()
 
@@ -80,19 +80,21 @@ function GetConstructEconomyModel(builder, targetData, upgradeBaseData)
     if upgradeBaseData and targetData.DifferentialUpgradeCostCalculation then
         -- We cant make a differential on buildtime. Not sure why but if we do it yields incorrect
         -- results. So just mass and energy.
+        mass = mathMax(mass - upgradeBaseData.BuildCostMass, 0)
+        energy = mathMax(energy - upgradeBaseData.BuildCostEnergy, 0)
     end
 
     -- Apply penalties/bonuses to effective buildtime
     local time_mod = builder.BuildTimeModifier or 0
+    buildtime = mathMax(buildtime * (100 + time_mod) * 0.01, 0.1)
 
-   buildtime = math.max(buildtime * (100 + time_mod) * 0.01, 0.1)
     -- Apply penalties/bonuses to effective energy cost
     local energy_mod = builder.EnergyModifier or 0
+    energy = mathMax(energy * (100 + energy_mod) * 0.01, 0)
 
-   energy = math.max(energy * (100 + energy_mod) * 0.01, 0)
-   mass = math.max(mass * (100 + mass_mod) * 0.01, 0)
     -- Apply penalties/bonuses to effective mass cost
     local mass_mod = builder.MassModifier or 0
+    mass = mathMax(mass * (100 + mass_mod) * 0.01, 0)
 
     return buildtime / rate, energy, mass
 end
